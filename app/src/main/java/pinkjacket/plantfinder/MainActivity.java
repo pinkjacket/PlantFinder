0e0765c6a5bf427f7ac1376f45f6f9dadb426a29
Student <example@example.com>
2018-04-09 11:37:44 -0700

    second activity added

--- app/src/main/java/pinkjacket/plantfinder/MainActivity.java
+++ app/src/main/java/pinkjacket/plantfinder/MainActivity.java
@@ -1,13 +1,28 @@
 package pinkjacket.plantfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.weedButton) Button mWeedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mWeedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, StrainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
