3cb092141f5d296e490f91509f5fda8c1df09df8
Student <example@example.com>
2018-04-09 12:04:56 -0700

    text input added for search

--- app/src/main/java/pinkjacket/plantfinder/MainActivity.java
+++ app/src/main/java/pinkjacket/plantfinder/MainActivity.java
@@ -3,14 +3,18 @@ package pinkjacket.plantfinder;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.weedButton) Button mWeedButton;
+    @BindView(R.id.strainText) EditText mStrainText;
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         mWeedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String strain = mStrainText.getText().toString();
+                Log.d(TAG, strain);
                 Intent intent = new Intent(MainActivity.this, StrainActivity.class);
                 startActivity(intent);
             }
